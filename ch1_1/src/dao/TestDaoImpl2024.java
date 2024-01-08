a99ed47fb71e802a4d18df67462aa07c51859b81
buleJeans <12540314+bulejeans@user.noreply.gitee.com>
2024-01-08 19:34:28 +0800

    init

---
+++ ch1_1/src/dao/TestDaoImpl2024.java
@@ -0,0 +1,11 @@
+package dao;
+
+public class TestDaoImpl2024 implements TestDao{
+    @Override
+    public void sayHello() {
+//		System.out.println("Learning without thinking is futile; thinking without learning is dangerous");
+        System.out.println("Hello 2024!");
+
+    }
+}
+
