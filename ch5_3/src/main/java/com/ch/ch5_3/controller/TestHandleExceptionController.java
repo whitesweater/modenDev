92b6f1566f23c725fa478703c819c334cd603ceb
buleJeans <12540314+bulejeans@user.noreply.gitee.com>
2024-01-08 20:18:59 +0800

    ini

--- ch5_3/src/main/java/com/ch/ch5_3/controller/TestHandleExceptionController.java
+++ ch5_3/src/main/java/com/ch/ch5_3/controller/TestHandleExceptionController.java
@@ -1,27 +1,32 @@
 package com.ch.ch5_3.controller;
+
 import java.sql.SQLException;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ch.ch5_3.exception.MyException;
+
 @Controller
-public class TestHandleExceptionController{
-	@RequestMapping("/")
-	public String index() {
-		return "index";
-	}
-	@RequestMapping("/db")  
-    public void db() throws SQLException { 
+public class TestHandleExceptionController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/db")
+    public void db() throws SQLException {
         throw new SQLException("数据库异常");
-    }  
-	@RequestMapping("/my")  
-    public void my() throws MyException {  
+    }
+
+    @RequestMapping("/my")
+    public void my() throws MyException {
         throw new MyException("自定义异常");
     }
-	@RequestMapping("/no")  
-    public void no() throws Exception {  
+
+    @RequestMapping("/no")
+    public void no() throws Exception {
         throw new Exception("未知异常");
-    } 
-	
+    }
+
 }
