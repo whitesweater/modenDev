72896166b40bff2f5314fce6bd76b0570c22b297
buleJeans <12540314+bulejeans@user.noreply.gitee.com>
2024-01-08 16:20:19 +0800

    Initial commit init

--- spring_boot_addstarters/src/main/java/com/ch/spring_boot_addstarters/SpringBootAddstartersApplication.java
+++ spring_boot_addstarters/src/main/java/com/ch/spring_boot_addstarters/SpringBootAddstartersApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootAddstartersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAddstartersApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAddstartersApplication.class, args);
+
+    }
 
 }
